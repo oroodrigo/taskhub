98faff9d694153bd40151087f30b908d7fb0ae41
Rodrigo Oliveira <rodrigofeitosa.contato@gmail.com>
2024-02-27 20:24:54 -0300

    feat: Create analytics page

---
+++ src/app/(task)/analytics/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Layout } from "@/components/ui/Layout";
+
+export default function Analytics() {
+  return (
+    <Layout title="Pagina de Analytics">
+      <main className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
+        Analytics
+      </main>
+    </Layout>
+  );
+}
