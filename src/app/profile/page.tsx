6171b3d01e452d559f8b0e4fdb8fec601c9e5444
Rodrigo Oliveira <rodrigofeitosa.contato@gmail.com>
2024-02-27 20:10:09 -0300

    feat: Create profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Layout } from "@/components/ui/Layout";
+
+export default function Profile() {
+  return (
+    <Layout title="Pagina de personalizaçao!">
+      <main className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
+        Personalize seu perfil!
+      </main>
+    </Layout>
+  );
+}
